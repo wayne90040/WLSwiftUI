72e1a0744154979a3cbf1720dfa5060a3d85a756
HSUWEILUN <wayne90040@gmail.com>
2024-06-17 23:34:30 +0800

    [Feature] Add `SizeReader`, `RoundedCorner`

---
+++ Sources/WLSwiftUI/SizeReaderView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+public struct SizeReaderView: View {
+    public let sizeChanged: (CGSize) -> Void
+    public var body: some View {
+        GeometryReader { reader in
+            Color.clear
+                .onAppear {
+                    sizeChanged(reader.size)
+                }
+                .onChange(of: reader.size) {
+                    sizeChanged($0)
+                }
+        }
+    }
+}
+
+public struct SizeReaderViewModifier: ViewModifier {
+    private let sizeChanged: (CGSize) -> Void
+    
+    public init(sizeChanged: @escaping (CGSize) -> Void) {
+        self.sizeChanged = sizeChanged
+    }
+    
+    public func body(content: Content) -> some View {
+        content
+            .background(SizeReaderView(sizeChanged: sizeChanged))
+    }
+}
+
+public extension View {
+    public func sizeChanged(sizeChanged: @escaping (CGSize) -> Void) -> some View {
+        modifier(SizeReaderViewModifier(sizeChanged: sizeChanged))
+    }
+}
