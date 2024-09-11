47c8bd28f571c63ff58f9c0e278f26c574112463
HSUWEILUN <wayne90040@gmail.com>
2024-09-11 23:35:50 +0800

    [Feature] add `onAsyncDidLoad`, transparent background, and  hide keyboard

---
+++ Sources/WLSwiftUI/View+Extensions.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+#if canImport(UIKit)
+public extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
